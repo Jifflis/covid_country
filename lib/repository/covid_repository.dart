365d4c8daf73344499a6b49f07e2ced7aa7c14e3
Jifflis <jifflisotomier@gmail.com>
2021-07-26 17:03:31 +0800

    Remove print

--- lib/repository/covid_repository.dart
+++ lib/repository/covid_repository.dart
@@ -7,19 +7,21 @@ import 'package:http/http.dart' as http;
 import '../env.dart';
 import 'api.dart';
 
-class CovidRepository{
-  factory CovidRepository(ApiProvider apiProvider)=>CovidRepository._(apiProvider);
+class CovidRepository {
+  factory CovidRepository(ApiProvider apiProvider) =>
+      CovidRepository._(apiProvider);
 
   CovidRepository._(this.api);
+
   ApiProvider api;
 
-  Future<List<CovidCountry>> getInfectedCountries() async{
+  Future<List<CovidCountry>> getInfectedCountries() async {
     final http.Response response = await api.get(Env.covidCountryUrl);
-    final List<dynamic> mapList = json.decode(response.body)??[];
+    final List<dynamic> mapList = json.decode(response.body) ?? [];
 
     final List<CovidCountry> covidCountries = <CovidCountry>[];
 
-    for(final Map<String,dynamic> map in mapList){
+    for (final Map<String, dynamic> map in mapList) {
       final CovidCountry country = CovidCountry.fromJson(map);
       covidCountries.add(country);
     }
@@ -27,18 +29,15 @@ class CovidRepository{
     return covidCountries;
   }
 
+  Future<CovidCountry> getInfectedCountry(String? url) async {
+    final http.Response response = await api.get(url ?? '');
+    final Map<String, dynamic> data = json.decode(response.body);
 
-  Future<CovidCountry> getInfectedCountry(String? url) async{
-    print(url);
-    final http.Response response = await api.get(url??'');
-    final Map<String,dynamic> data = json.decode(response.body);
-
-    print(data.toString());
     CovidCountry country = CovidCountry.fromJson(data);
-    final List<dynamic> mapList = data['infectedByRegion']??[];
+    final List<dynamic> mapList = data['infectedByRegion'] ?? [];
 
     List<InfectedRegion> regions = [];
-    for(final Map<String,dynamic> map in mapList){
+    for (final Map<String, dynamic> map in mapList) {
       final InfectedRegion region = InfectedRegion.fromJson(map);
       regions.add(region);
     }
